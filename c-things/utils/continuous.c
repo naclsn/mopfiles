edf99351e66a121a1da955abe56f202872d740c7
Grenier Celestin <celestin.grenier@hotmail.com>
2024-05-12 10:59:15 +0200

    co_parallel

--- c-things/utils/continuous.c
+++ c-things/utils/continuous.c
@@ -28,6 +28,9 @@
 
 #ifndef co_define
 
+#define _CO_JOIN(a, b) a##b
+#define _CO_XJOIN(a, b) _CO_JOIN(a, b)
+
 /// vaargs should be the state structure, after should be the function body
 #define co_define(__name, ...)              \
     struct __name##_state { __VA_ARGS__ };  \
@@ -37,22 +40,39 @@
     int __name(struct __name##_state* const)
 
 /// vaargs should be some `co_seq_item`s
-#define co_sequence(__seqst, ...) do {  \
-        int* const _seqst = (__seqst);  \
-        switch (*_seqst) {              \
-            char const _pk;             \
-            __VA_ARGS__;                \
-            return 0;                   \
-        }                               \
+#define co_sequence(__seqst, ...) do {     \
+        size_t* const _seqst = (__seqst);  \
+        switch (*_seqst) {                 \
+            char const _pk;                \
+            __VA_ARGS__;                   \
+            return 0;                      \
+        }                                  \
     } while (0)
 /// vaargs should be an expression that is non-zero if moving to the next state
 #define co_seq_item(...)                                     \
         case sizeof(struct _k { char _[sizeof _pk+1]; })-2:  \
             for (struct _k const _pk = {{*_seqst+= !!(__VA_ARGS__)}} ;0; (void)_pk)
 
+/// vaargs should be some `co_par_item`s
+#define co_parallel(__parst, ...) do {                         \
+        size_t* const _parst = (__parst);                      \
+        char const _pk;                                        \
+        __VA_ARGS__                                            \
+        if ((1ull<<(sizeof _pk-1))-1ull != *_parst) return 0;  \
+        else goto _CO_XJOIN(_pass_, __LINE__);  \
+    _CO_XJOIN(_pass_, __LINE__):;  \
+    } while (0)
+/// vaargs should be an expression that is non-zero if it is done
+#define co_par_item(...)                                      \
+        for (struct { char _[sizeof _pk+1]; } const _pk = {{  \
+                !(*_parst>>(sizeof _pk-2)&1) &&               \
+                !!(__VA_ARGS__) &&                            \
+                (*_parst|= 1<<(sizeof _pk-2))                 \
+            }} ;3; (void)_pk)
+
 /// vaargs should be an expression that is non-zero if resetting
 #define co_repeat(__repst, __count, __itst, ...) do {  \
-        int* const _repst = (__repst);                 \
+        size_t* const _repst = (__repst);              \
         if (*_repst < (__count)) {                     \
             if (__VA_ARGS__) {                         \
                 ++*_repst;                             \
@@ -87,7 +107,7 @@ co_define(seq_example,
     struct dos_state dos_st;
     struct tres_state tres_st;
     struct tres_state tres_st_2;
-    int seqst;
+    size_t seqst;
 ) {
     co_sequence (&state->seqst,
         co_seq_item(tres(&state->tres_st))
@@ -99,7 +119,7 @@ co_define(seq_example,
 }
 
 co_define(rep_example,
-    int k;
+    size_t k;
     int const n;
     struct seq_example_state bdf;
 ) {
@@ -107,6 +127,19 @@ co_define(rep_example,
     return 1;
 }
 
+co_define(par_example,
+    size_t parst;
+    struct dos_state dos_st;
+    struct tres_state tres_st;
+) {
+    co_parallel (&state->parst,
+        co_par_item(dos(&state->dos_st))
+        co_par_item(tres(&state->tres_st))
+    );
+    return 1;
+}
+
 int main(void) {
-    co_consume(rep_example, .n= 3) puts("---");
+    //co_consume(rep_example, .n= 3) puts("---");
+    co_consume(par_example, 0) puts("---");
 }
