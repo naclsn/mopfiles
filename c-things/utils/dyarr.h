e0c6a80a016d80aa5e338355cd924d53935e5b42
Grenier Celestin <celestin.grenier@hotmail.com>
2024-05-28 15:30:26 +0200

    wip streaming lexer

--- c-things/utils/dyarr.h
+++ c-things/utils/dyarr.h
@@ -11,10 +11,10 @@
 #ifndef arry
 #define arry(...) struct { __VA_ARGS__* ptr; size_t len, cap; }
 #define frry(__a) ((__a)->cap ? free((__a)->ptr) : (void)0, (__a)->ptr = NULL, (__a)->len = (__a)->cap = 0)
-#define push(__a) ((__a)->len < (__a)->cap || ((__a)->cap && ((__a)->ptr = realloc((__a)->ptr, ((__a)->cap+= (__a)->cap+8)*sizeof*(__a)->ptr))) ? (__a)->ptr+(__a)->len++ : (OOM))
+#define push(__a) ((__a)->len < (__a)->cap || (((__a)->cap || !(__a)->ptr) && ((__a)->ptr = realloc((__a)->ptr, ((__a)->cap+= (__a)->cap+8)*sizeof*(__a)->ptr))) ? (__a)->ptr+(__a)->len++ : (OOM))
 #define grow(__a, __k, __n) _grow((char**)&(__a)->ptr, &(__a)->len, &(__a)->cap, sizeof*(__a)->ptr, (__k), (__n))
 #define last(__a) ((__a)->ptr+(__a)->len-1)
-#define each(__a) for (void* const _ptr = (__a)->ptr,* const _end = _ptr+(__a)->len; _end == (__a)->ptr ? (__a)->ptr = _ptr, 0 : 1; ++(__a)->ptr)
+#define each(__a) for (void* const _ptr = (__a)->ptr,* const _end = (__a)->ptr+(__a)->len; _end == (__a)->ptr ? (__a)->ptr = _ptr, 0 : 1; ++(__a)->ptr)
 static inline void* _grow(char** const ptr, size_t* const len, size_t* const cap, size_t const s, size_t const k, size_t const n) {
     size_t const nlen = *len+n;
     if (*cap < nlen && !(cap && (*ptr = realloc(*ptr, (*cap = nlen)*s)))) return (OOM);
@@ -34,15 +34,17 @@ static inline void* _grow(char** const ptr, size_t* const len, size_t* const cap
 #define _dyarr_emptyresize 8
 #endif
 
+#define _dyarr_inline_ns(__fname) _##__fname
+
 #if __STDC_VERSION__ < 199901L
 #define inline
 #endif
 
-static inline bool  _dyarr_resize(void** ptr, size_t isz, size_t* cap, size_t rsz);
-static inline void* _dyarr_insert(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n);
-static inline void  _dyarr_remove(void** ptr, size_t isz, size_t* len, size_t k, size_t n);
-static inline void* _dyarr_replace(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n, void* spt, size_t sln);
-static inline int   _dyarr_cmp(void** s1, size_t z1, void** s2, size_t z2);
+static inline bool  _dyarr_inline_ns(dyarr_resize)(void** ptr, size_t isz, size_t* cap, size_t rsz);
+static inline void* _dyarr_inline_ns(dyarr_insert)(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n);
+static inline void  _dyarr_inline_ns(dyarr_remove)(void** ptr, size_t isz, size_t* len, size_t k, size_t n);
+static inline void* _dyarr_inline_ns(dyarr_replace)(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n, void* spt, size_t sln);
+static inline int   _dyarr_inline_ns(dyarr_cmp)(void** s1, size_t z1, void** s2, size_t z2);
 
 #ifdef inline
 #undef inline
@@ -77,33 +79,33 @@ static inline int   _dyarr_cmp(void** s1, size_t z1, void** s2, size_t z2);
 /* same as memcmp as for value sign, also compares lengths first in same way */
 #define dyarr_cmp(__s1, __s2)  _dyarr_cmp((void**)(__s1)->ptr, (__s1)->len*sizeof*(__s1)->ptr, (void**)(__s2)->ptr, (__s2)->len*sizeof*(__s2)->ptr)
 
-bool _dyarr_resize(void** ptr, size_t isz, size_t* cap, size_t rsz) {
+bool _dyarr_inline_ns(dyarr_resize)(void** ptr, size_t isz, size_t* cap, size_t rsz) {
     void* niw = realloc(*ptr, rsz * isz);
     return niw ? *ptr = niw, *cap = rsz, true : false;
 }
 
-void* _dyarr_insert(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n) {
+void* _dyarr_inline_ns(dyarr_insert)(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n) {
     size_t nln = *len+n;
-    if (*cap < nln && !_dyarr_resize(ptr, isz, cap, nln)) return NULL;
+    if (*cap < nln && !_dyarr_inline_ns(dyarr_resize)(ptr, isz, cap, nln)) return NULL;
     memmove(*(char**)ptr+(k+n)*isz, *(char**)ptr+k*isz, (*len-k)*isz);
     *len = nln;
     return *(char**)ptr+k*isz;
 }
 
-void _dyarr_remove(void** ptr, size_t isz, size_t* len, size_t k, size_t n) {
+void _dyarr_inline_ns(dyarr_remove)(void** ptr, size_t isz, size_t* len, size_t k, size_t n) {
     memmove(*(char**)ptr+k*isz, *(char**)ptr+(k+n)*isz, ((*len-= n)-k)*isz);
 }
 
-void* _dyarr_replace(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n, void* spt, size_t sln) {
+void* _dyarr_inline_ns(dyarr_replace)(void** ptr, size_t isz, size_t* cap, size_t* len, size_t k, size_t n, void* spt, size_t sln) {
     size_t nln = *len+sln-n;
-    if (n < sln && *cap < nln && !_dyarr_resize(ptr, isz, cap, nln)) return NULL;
+    if (n < sln && *cap < nln && !_dyarr_inline_ns(dyarr_resize)(ptr, isz, cap, nln)) return NULL;
     memmove(*(char**)ptr+(k+sln)*isz, *(char**)ptr+(k+n)*isz, (*len-n-k)*isz);
     memcpy(*(char**)ptr+k*isz, spt, sln*isz);
     *len = nln;
     return *(char**)ptr+k*isz;
 }
 
-int _dyarr_cmp(void** s1, size_t z1, void** s2, size_t z2) {
+int _dyarr_inline_ns(dyarr_cmp)(void** s1, size_t z1, void** s2, size_t z2) {
     int d = z1-z2;
     return d ? d : memcmp(s1, s2, z1);
 }
