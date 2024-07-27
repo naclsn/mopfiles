39c894c7295c682c40ff2d1bd7206fe8cf569e3e
Grenier Celestin <celestin.grenier@hotmail.com>
2024-07-27 13:07:54 +0200

    regs, line edit

--- c-things/uh.c
+++ c-things/uh.c
@@ -8,6 +8,8 @@
 #include <termios.h>
 #include <unistd.h> // read/write
 
+#define countof(__a) (sizeof(__a)/sizeof*(__a))
+
 // arry {{{
 #define arry(...) struct { __VA_ARGS__* ptr; size_t len, cap; }
 #define frry(__a) (free((__a)->cap ? (__a)->ptr : NULL), (__a)->ptr = NULL, (__a)->len = (__a)->cap = 0)
@@ -93,29 +95,50 @@ struct {
     }) bufs;
 
     size_t vis_buf;
-    arry(char) typeahead;
+    arry(char) typeahead; // TODO: make it a reg
+
+    char* regs[127]; // TODO: make it tighter
 
     short unsigned rows, cols;
 
-    struct state {
+    arry(struct state {
         enum {
             STARTING,
             QUITTING,            // extra: status
-            NEED_RESIZE,         // extra: next
-            NEED_REDRAW,         // extra: next
+            NEED_RESIZE,
+            NEED_REDRAW,
+            NEED_RESTATUS,
+            NEED_REPOS,
             USER_PENDING,
-            USER_READLINE_EN,    // extra: next
-            USER_READLINE_TY,    // extra: next
-            USER_READLINE_EX,    // extra: next
+            USER_READLINE,       // extra: char
         } tag;
         size_t extra;
-    } state;
+    }) states;
 } uh = {0};
 
 #define visible (uh.bufs.ptr+uh.vis_buf)
 #define markk(__c) (visible->marks+char_to_mark(__c))
 #define cursor markk('.')
-#define xstate(__s, __x) (uh.state = (struct state){(__s), (__x)})
+
+#define cstate last(&uh.states)
+#define understate(__s, __x) memcpy(grow(&uh.states, 0, 1), &(struct state){(__s), (__x)}, sizeof(struct state))
+#define abovestate(__s, __x) (*push(&uh.states) = (struct state){(__s), (__x)})
+#define popstate() (uh.states.ptr[--uh.states.len])
+#define replstate(__s, __x) (*cstate = (struct state){(__s), (__x)})
+// }}}
+
+// term/cursor functions {{{
+#define cu_enter_alt() fprintf(term, "\x1b[?1049h")
+#define cu_leave_alt() fprintf(term, "\x1b[?1049l")
+
+void cu_goto(short unsigned row, short unsigned col) {
+    fprintf(term, "\x1b[%hu;%huH", row+1, col+1);
+}
+
+void repos_cursor(void) {
+    static unsigned const gutter_width = 5; // "%4zu "
+    cu_goto(cursor->ln, cursor->ch+gutter_width);
+}
 // }}}
 
 // somewhat misc {{{
@@ -127,8 +150,12 @@ void cleanup(void) {
     frry(&uh.bufs);
 
     frry(&uh.typeahead);
-    struct _ { char exhaustive_cleanup_check[80 == sizeof uh ? 1 : -1]; };
+    for (size_t k = 0; k < countof(uh.regs); ++k) free(uh.regs[k]);
+    struct _ { char exhaustive_cleanup_check[1104 == sizeof uh ? 1 : -1]; };
 
+    cu_leave_alt();
+    fputc('\r', term);
+    fflush(term);
     set_ini_tios();
 }
 
@@ -136,8 +163,8 @@ void signaled(int const signum) {
 #define __do(__s) _##__s= __s,
     switch ((enum { caught_sigs(__do) })signum) {
 #undef __do
-    case _SIGINT:   xstate(QUITTING, 2);               break;
-    case _SIGWINCH: xstate(NEED_RESIZE, uh.state.tag); break;
+    case _SIGINT:   replstate(QUITTING, 2);     break;
+    case _SIGWINCH: abovestate(NEED_RESIZE, 0); break;
     }
 }
 
@@ -152,7 +179,7 @@ char getk(void) {
 
 bool loadbuf(char const* const path, struct buf* const res) {
     *res = (struct buf){.name= path};
-    FILE* const f = fopen(path, "rb");
+    FILE* const f = fopen(path, "r");
     if (!f) return 0;
     size_t alen;
     char* aptr;
@@ -220,62 +247,85 @@ bool move_next_char(bool* const nl) {
 }
 // }}}
 
-// term/cursor functions, redraw functions {{{
-void cu_goto(short unsigned row, short unsigned col) {
-    fprintf(term, "\x1b[%hu;%huH", row+1, col+1);
-}
+// line editing {{{
+void readline(char const r) {
+    fprintf(term, "%c%s", r, uh.regs[(int)r] ? uh.regs[(int)r] : "");
 
-void repos_cursor(void) {
-    static unsigned const gutter_width = 5; // "%4zu "
-    cu_goto(cursor->ln, cursor->ch+gutter_width);
-}
+    size_t at = 0;
+    arry(char) edit = {0};
+    if (uh.regs[(int)r]) {
+        edit.len = edit.cap = (at = strlen(edit.ptr = uh.regs[(int)r]))+1;
+        uh.regs[(int)r] = NULL;
+    } else *push(&edit) = '\0';
 
-void redraw_status(void) {
-    cu_goto(uh.rows-2, 0);
+    char c;
+    while (1) switch (fflush(term), c = getk()) default: {
+        if (' ' <= c && c <= '~') {
+            if (edit.len-1 == at++) fputc(push(&edit)[-1] = c, term), *last(&edit) = '\0';
+            else fprintf(term, "\x1b[@%c", *grow(&edit, at-1, 1) = c);
+        }
 
-    char posinfo[64];
-    int const posinfo_len = sprintf(posinfo, "%zu/%zu,%zu",
-            cursor->ln+1, visible->text.len, cursor->ch+1);
-    int const nameinfo_len = fprintf(term, "%s%s%s%s",
-            visible->name ? visible->name : "[Scratch]",
-            visible->flags.modified ? "+" : "",
-            visible->flags.exists ? "" : "[NEW]",
-            visible->flags.readonly ? "[RO]" : "");
-    fprintf(term, "%*s%s\r\n",
-            uh.cols-nameinfo_len-posinfo_len, "",
-            posinfo);
-}
+        if (0) case CTRL('A'): fprintf(term, "\r%c", r), at = 0;
+        if (0) case CTRL('B'): if (at) --at, fputc('\b', term);
+        if (0) case CTRL('C'): { frry(&edit); return; }
+        if (0) case CTRL('D'): if (1 < edit.len && at < edit.len-2) grow(&edit, at+1, -1), fprintf(term, "\x1b[P");
+        if (0) case CTRL('E'): if (at < edit.len-1) fprintf(term, "\x1b[%dC", (unsigned)(edit.len-1-at)), at = edit.len-1;
+        if (0) case CTRL('F'): if (at < edit.len-1) fputc(edit.ptr[at++], term);
 
-void redraw_screen(void) {
-    static unsigned const gutter_width = 5; // "%4zu "
+        if (0) case CTRL('H'): case 127: if (at) {
+            if (edit.len-1 == at--) edit.ptr[--edit.len-1] = '\0';
+            else grow(&edit, at+1, -1);
+            fprintf(term, "\b\x1b[P");
+        }
 
-    cu_goto(0, 0);
+        //if (0) case CTRL('I'): { } // compl?
 
-    size_t const first = markk('(')->ln, last = visible->text.len;
-    size_t k_scr, k_txt;
-    for (k_scr = k_txt = 0; k_scr+2 < uh.rows; k_scr++) {
-        if (last <= k_txt) break;
-        size_t const full = visible->text.ptr[k_txt].len;
-        unsigned const fits = full < uh.cols-gutter_width ? full : uh.cols-gutter_width;
-        fprintf(term, "%4zu %.*s\r\n", k_scr+1, fits, visible->text.ptr[k_txt].ptr);
-        ++k_txt;
+        if (0) case CTRL('J'): case CTRL('M'): { uh.regs[(int)r] = edit.ptr; return; }
+
+        if (0) case CTRL('K'): if (at < edit.len-1) fprintf(term, "\x1b[K"), edit.ptr[edit.len = at] = '\0';
+        //if (0) case CTRL('L'): { } // dig? if not on ^K...
+
+        //if (0) case CTRL('N'): { }
+        //if (0) case CTRL('O'): { }
+        //if (0) case CTRL('P'): { }
+
+        if (0) case CTRL('R'): {
+            switch (c = getk()) default: {
+                if (uh.regs[(int)c]) {
+                    size_t const len = strlen(uh.regs[(int)c]);
+                    memcpy(grow(&edit, at, len), uh.regs[(int)c], len);
+                }
+
+                //if (0) case '=': {
+                //    fprintf(term, "\r\x1b[K");
+                //    readline(rr);
+                //    // exec
+                //    // reprint
+                //    // reposition
+                //    // insert as below
+                //}
+            }
+        }
+
+        if (0) case CTRL('U'): if (at) grow(&edit, at, -at), fprintf(term, "\x1b[%uD\x1b[%uP", (unsigned)at, (unsigned)at), at = 0;
+
+        if (0) case CTRL('['): switch (c = getk()) default: {
+
+            if (0) case CTRL('['): { frry(&edit); return; }
+        }
     }
-    markk(')')->ln = first+k_scr;
-
-    while (k_scr+2 < uh.rows) fprintf(term, "%4zu~\r\n", ++k_scr);
-
-    redraw_status();
 }
 // }}}
 
 // switch key functions {{{
-void movment_key(char const key) {
+bool movment_key(char const key) {
     size_t *const ln = &cursor->ln, *const ch = &cursor->ch;
 #   define mln (visible->text.len)
 #   define mch (visible->text.ptr[*ln].len)
 #   define atc (visible->text.ptr[*ln].ptr[*ch])
 
     switch (key) default: {
+        return false;
 
         if (0) {
             if (0) case 'h': if (*ch)         --*ch;
@@ -294,7 +344,6 @@ void movment_key(char const key) {
 #       define blankchar(__c) (' ' == (__c) || '\t' == (__c))
 
         if (0) {
-
             bool nl;
 
             if (0) case 'w': case 'W': {
@@ -327,7 +376,12 @@ void movment_key(char const key) {
                 while ((outnotmv = move_prev_char(&nl)) && !nl && (key&32 ? firstwc == wordchar(atc) : !blankchar(atc)));
                 if (outnotmv) move_next_char(&nl);
             }
+        }
 
+        if (0) {
+            bool nl;
+            if (0) case ' ':                 move_next_char(&nl);
+            if (0) case 127: case CTRL('H'): move_prev_char(&nl);
         }
 
         if (0) case '0': *ch = 0;
@@ -342,27 +396,103 @@ void movment_key(char const key) {
 #   undef mch
 #   undef mln
 #   undef atc
+
+    return true;
 }
 
-void normal_key(char const key) {
+bool normal_key(char const key) {
     switch (key) default: {
-        movment_key(key);
-        redraw_status();
-        repos_cursor();
+        if (movment_key(key)) {
+            abovestate(NEED_RESTATUS, 0);
+            return true;
+        }
+        return false;
+
+        if (0) {
+            if (0) case 'c': ;
+            if (0) case 'd': ;
+            if (0) case 'y': ;
+
+            if (!movment_key(getk())) return false;
+            // TODO: resolve or smth
+
+            switch (key) {
+            case 'c':
+                cu_goto(uh.rows-1, 0);
+                readline('.');
+                break; // TODO
+            case 'd':
+                break;
+            case 'y':
+                break; // TODO
+            }
+        }
 
         if (0) {
         case CTRL('Z'): kill(0, SIGTSTP);
-        case CTRL('L'): xstate(NEED_REDRAW, uh.state.tag);
+        case CTRL('L'): abovestate(NEED_REDRAW, 0);
+        }
+
+        if (0) case CTRL('C'): replstate(QUITTING, 0);
+
+        if (0) {
+            if (0) case CTRL('E'): if (markk('(')->ln < visible->text.len-1) {
+                ++markk('(')->ln;
+                fprintf(term, "\x1b[S\x1b[A");
+            }
+            if (0) case CTRL('Y'): if (markk('(')->ln) {
+                --markk('(')->ln;
+                fprintf(term, "\x1b[T\n");
+            }
+            abovestate(NEED_REPOS, 0);
         }
 
         if (0) {
-        case CTRL('C'): xstate(QUITTING, 0);
-        }
-
-        if (0) {
-        case ':': case '/': case '?': xstate(USER_READLINE_EN, key);
+        case ':': case '/': case '?': abovestate(USER_READLINE, key);
         }
     }
+
+    return true;
+}
+// }}}
+
+// redraw functions {{{
+void redraw_status(void) {
+    cu_goto(uh.rows-2, 0);
+
+    char posinfo[64];
+    int const posinfo_len = sprintf(posinfo, "%zu/%zu,%zu",
+            cursor->ln+1, visible->text.len, cursor->ch+1);
+    int const nameinfo_len = fprintf(term, "%s%s%s%s",
+            visible->name ? visible->name : "[Scratch]",
+            visible->flags.modified ? "+" : "",
+            visible->flags.exists ? "" : "[NEW]",
+            visible->flags.readonly ? "[RO]" : "");
+    fprintf(term, "%*s%s\r\n",
+            uh.cols-nameinfo_len-posinfo_len, "",
+            posinfo);
+}
+
+void redraw_screen(void) {
+    static unsigned const gutter_width = 5; // "%4zu "
+
+    cu_goto(0, 0);
+
+    size_t const top = markk('(')->ln, last = visible->text.len;
+    size_t k_scr, k_txt;
+    for (k_scr = k_txt = 0; k_scr+2 < uh.rows; k_scr++) {
+        if (last <= k_txt) break;
+        size_t const full = visible->text.ptr[k_txt].len;
+        unsigned const fits = full < uh.cols-gutter_width ? full : uh.cols-gutter_width;
+        fprintf(term, "%4zu %.*s\r\n", k_scr+1, fits, visible->text.ptr[k_txt].ptr);
+        ++k_txt;
+    }
+    size_t const bot = markk(')')->ln = top+k_scr;
+
+    if (cursor->ln < top) cursor->ln = top+2;
+    if (bot < cursor->ln) cursor->ln = bot-2;
+
+    while (k_scr+2 < uh.rows) fprintf(term, "%4zu~\r\n", ++k_scr);
 }
 // }}}
 
@@ -371,21 +501,25 @@ int main(int argc, char** argv) {
 
     if (!setup_sigs() || !setup_tios()) return EXIT_FAILURE;
     set_raw_tios();
+    cu_enter_alt();
     atexit(cleanup);
 
-    struct buf* const land = push(&uh.bufs);
-    if (argc && loadbuf((argc--, *argv++), land))
-        land->flags.exists = true;
+    understate(STARTING, 0);
 
-    while (1) switch (uh.state.tag) default: {
-        xstate(QUITTING, 3);
+    while (1) switch (cstate->tag) default: {
+        replstate(QUITTING, 3);
 
         if (0) case STARTING: {
-            xstate(NEED_RESIZE, USER_PENDING);
+            struct buf* const land = push(&uh.bufs);
+            if (argc && loadbuf((argc--, *argv++), land)) land->flags.exists = true;
+
+            popstate();
+            abovestate(USER_PENDING, 0);
+            abovestate(NEED_RESIZE, 0);
         }
 
         if (0) case QUITTING: {
-            exit(uh.state.extra);
+            exit(cstate->extra);
         }
 
         if (0) case NEED_RESIZE: {
@@ -396,10 +530,10 @@ int main(int argc, char** argv) {
             while (read(TERM, &c, 1), ';' != c) uh.rows = uh.rows*10 + c-'0';
             while (read(TERM, &c, 1), 'R' != c) uh.cols = uh.cols*10 + c-'0';
 
-        case NEED_REDRAW:
-            redraw_screen();
-            repos_cursor();
-            xstate(uh.state.extra, 0);
+        case NEED_REDRAW:   redraw_screen();
+        case NEED_RESTATUS: redraw_status();
+        case NEED_REPOS:    repos_cursor();
+            popstate();
         }
 
         if (0) case USER_PENDING: {
@@ -407,14 +541,11 @@ int main(int argc, char** argv) {
             normal_key(getk());
         }
 
-        if (0) case USER_READLINE_EN: {
+        if (0) case USER_READLINE: {
             cu_goto(uh.rows-1, 0);
-            fputc(uh.state.extra, term);
-            xstate(USER_READLINE_TY, uh.state.extra);
-        }
-
-        if (0) case USER_READLINE_TY: {
-            xstate(QUITTING, 42); // NIY
+            readline(cstate->extra);
+            fprintf(term, "[[!!%s!!]]", uh.regs[(int)cstate->extra]);
+            popstate();
         }
     }
 
