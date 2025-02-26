05fe658a9130a383ddf30cb7fc472c79abf83c60
Grenier Celestin <celestin.grenier@hotmail.com>
2025-02-26 23:55:57 +0100

    lsdjf

--- idk/skyblock/python.py
+++ idk/skyblock/python.py
@@ -1,10 +1,23 @@
+from codeop import compile_command
+from types import FunctionType
 import readline as _
 
-w = {}
+g = {"__builtins__": {}}
 while 1:
+
     try:
-        print(eval(input("[@] "), {"__builtins__": {}}, w))
+        i = input("[@] ") + "\n"
+        c = compile_command(i)
+        if c is None:
+            for l in iter(lambda: input("    "), ""):
+                i += l + "\n"
+            c = compile_command(i)
+            assert c
+
+        r = FunctionType(c, g)()
+        r is not None and print(r)
+
     except EOFError:
         break
     except BaseException as e:
-        print(e)
+        print("!!!", e)
